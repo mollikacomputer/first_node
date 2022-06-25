8db72c1e62438e52cbff7dccdb794f0cce08b2f5
mollikacomputer <mollikacomputer3@gmail.com>
2022-06-25 07:50:16 +0600

    create first app for node  server

---
+++ __steps.js
@@ -0,0 +1,13 @@
+/*
+* 1. create a folder (manually or mkdir)
+* 2. open command line to that folder
+* 3. npm init
+* 4. npm install express
+* 5. create index.js
+
+----------------------------
+For express/ node server
+----------------------------
+* 1. 
+*
+*/
